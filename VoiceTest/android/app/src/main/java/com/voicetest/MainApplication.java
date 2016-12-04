6368bf9aaae04f0fb1428a0853b07b94dbaaf413
Brendan Moore <brendan@bfdmoore.com>
2016-12-04 17:00:24 -0500

    Update for newer RN and React Update Android app for latest React Native by creating MainApplication.java and moving the appropriate configuration over. Bump dependencies versions Bump release version number
    No changes were made to core voice package internals.

---
+++ VoiceTest/android/app/src/main/java/com/voicetest/MainApplication.java
@@ -0,0 +1,47 @@
+package com.voicetest;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+
+import com.wenkesj.voice.VoicePackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    /**
+     * Returns whether dev mode should be enabled.
+     * This enables e.g. the dev menu.
+     */
+    @Override
+    protected boolean getUseDeveloperSupport() {
+        return BuildConfig.DEBUG;
+    }
+
+    /**
+     * A list of packages used by the app. If the app uses additional views
+     * or modules besides the default ones, add more packages here.
+     */
+    @Override
+    protected List<ReactPackage> getPackages() {
+        return Arrays.<ReactPackage>asList(
+            new MainReactPackage(),
+            new VoicePackage()
+        );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+      return mReactNativeHost;
+  }
+}
