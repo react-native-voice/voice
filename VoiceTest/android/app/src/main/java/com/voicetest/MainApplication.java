0e10efa6615e1d62b865f932889b6357b204e1e0
Brendan Moore <brendan@bfdmoore.com>
2016-12-10 02:43:23 -0500

    Update README for required additions and possible permissions issue. Remove duplicate line.

--- VoiceTest/android/app/src/main/java/com/voicetest/MainApplication.java
+++ VoiceTest/android/app/src/main/java/com/voicetest/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
