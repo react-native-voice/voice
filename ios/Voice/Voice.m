adbf12d36a020447a38efaeb136855f048369e70
Louis <louis.hsiao@d8ai.com>
2017-12-20 13:29:22 +0800

    add rate change

--- ios/Voice/Voice.m
+++ ios/Voice/Voice.m
@@ -12,59 +12,63 @@
 @property (nonatomic) AVAudioEngine* audioEngine;
 @property (nonatomic) SFSpeechRecognitionTask* recognitionTask;
 @property (nonatomic) AVAudioSession* audioSession;
-
+@property (nonatomic) double rate;
 @end
 
 @implementation Voice
 {
 }
-
+- (id) init{
+    
+    self.rate = 0;
+    return self;
+}
 - (void) setupAndStartRecognizing:(NSString*)localeStr {
     [self teardown];
-
+    
     NSLocale* locale = nil;
     if ([localeStr length] > 0) {
         locale = [NSLocale localeWithLocaleIdentifier:localeStr];
     }
-
+    
     if (locale) {
         self.speechRecognizer = [[SFSpeechRecognizer alloc] initWithLocale:locale];
     } else {
         self.speechRecognizer = [[SFSpeechRecognizer alloc] init];
     }
-
+    
     self.speechRecognizer.delegate = self;
-
+    
     NSError* audioSessionError = nil;
     self.audioSession = [AVAudioSession sharedInstance];
-
+    
     if (audioSessionError != nil) {
         [self sendResult:RCTMakeError([audioSessionError localizedDescription], nil, nil) :nil :nil :nil];
         return;
     }
-
+    
     self.recognitionRequest = [[SFSpeechAudioBufferRecognitionRequest alloc] init];
-
+    
     if (self.recognitionRequest == nil){
         [self sendResult:RCTMakeError(@"Unable to created a SFSpeechAudioBufferRecognitionRequest object", nil, nil) :nil :nil :nil];
         return;
     }
-
+    
     if (self.audioEngine == nil) {
         self.audioEngine = [[AVAudioEngine alloc] init];
     }
-
+    
     AVAudioInputNode* inputNode = self.audioEngine.inputNode;
     if (inputNode == nil) {
         [self sendResult:RCTMakeError(@"Audio engine has no input node", nil, nil) :nil :nil :nil];
         return;
     }
-
+    
     // Configure request so that results are returned before audio recording is finished
     self.recognitionRequest.shouldReportPartialResults = YES;
-
+    
     [self sendEventWithName:@"onSpeechStart" body:@true];
-
+    
     // A recognition task represents a speech recognition session.
     // We keep a reference to the task so that it can be cancelled.
     self.recognitionTask = [self.speechRecognizer recognitionTaskWithRequest:self.recognitionRequest resultHandler:^(SFSpeechRecognitionResult * _Nullable result, NSError * _Nullable error) {
@@ -74,7 +78,7 @@
             [self teardown];
             return;
         }
-
+        
         BOOL isFinal = result.isFinal;
         if (result != nil) {
             NSMutableArray* transcriptionDics = [NSMutableArray new];
@@ -83,7 +87,7 @@
             }
             [self sendResult:nil:result.bestTranscription.formattedString :transcriptionDics :@(isFinal)];
         }
-
+        
         if (isFinal == YES) {
             if (self.recognitionTask.isCancelled || self.recognitionTask.isFinishing){
                 [self sendEventWithName:@"onSpeechEnd" body:@{@"error": @false}];
@@ -91,15 +95,33 @@
             [self teardown];
         }
     }];
-
-    AVAudioFormat* recordingFormat = [inputNode outputFormatForBus:0];
-
-    [inputNode installTapOnBus:0 bufferSize:1024 format:recordingFormat block:^(AVAudioPCMBuffer * _Nonnull buffer, AVAudioTime * _Nonnull when) {
+    AVAudioMixerNode* tempNode = [[AVAudioMixerNode alloc] init];
+    [self.audioEngine attachNode:tempNode];
+    [self.audioEngine connect:inputNode to:tempNode format:nil];
+    
+    AVAudioFormat* recordingFormat = [inputNode inputFormatForBus:0];
+    
+    if(self.rate == 0){
+        self.rate = [inputNode inputFormatForBus:0].sampleRate;
+    }
+    recordingFormat = [recordingFormat initStandardFormatWithSampleRate:self.rate channels:1];
+    
+    NSLog(@"lalala%@", recordingFormat);
+    
+    
+    [tempNode installTapOnBus:0 bufferSize:1024 format:recordingFormat block:^(AVAudioPCMBuffer * _Nonnull buffer, AVAudioTime * _Nonnull when) {
         if (self.recognitionRequest != nil) {
             [self.recognitionRequest appendAudioPCMBuffer:buffer];
         }
     }];
-
+    //    AVAudioFormat* recordingFormat = [inputNode outputFormatForBus:0];
+    //
+    //    [inputNode installTapOnBus:0 bufferSize:1024 format:recordingFormat block:^(AVAudioPCMBuffer * _Nonnull buffer, AVAudioTime * _Nonnull when) {
+    //        if (self.recognitionRequest != nil) {
+    //            [self.recognitionRequest appendAudioPCMBuffer:buffer];
+    //        }
+    //    }];
+    
     [self.audioEngine prepare];
     [self.audioEngine startAndReturnError:&audioSessionError];
     if (audioSessionError != nil) {
@@ -111,14 +133,14 @@
 - (NSArray<NSString *> *)supportedEvents
 {
     return @[
-        @"onSpeechResults",
-        @"onSpeechStart",
-        @"onSpeechPartialResults",
-        @"onSpeechError",
-        @"onSpeechEnd",
-        @"onSpeechRecognized",
-        @"onSpeechVolumeChanged"
-    ];
+             @"onSpeechResults",
+             @"onSpeechStart",
+             @"onSpeechPartialResults",
+             @"onSpeechError",
+             @"onSpeechEnd",
+             @"onSpeechRecognized",
+             @"onSpeechVolumeChanged"
+             ];
 }
 
 - (void) sendResult:(NSDictionary*)error :(NSString*)bestTranscription :(NSArray*)transcriptions :(NSNumber*)isFinal {
@@ -140,13 +162,13 @@
     [self.recognitionTask cancel];
     self.recognitionTask = nil;
     self.audioSession = nil;
-
+    
     if (self.audioEngine.isRunning) {
         [self.audioEngine stop];
         [self.recognitionRequest endAudio];
         [self.audioEngine.inputNode removeTapOnBus:0];
     }
-
+    
     self.recognitionRequest = nil;
 }
 
@@ -205,7 +227,7 @@ RCT_EXPORT_METHOD(startSpeech:(NSString*)localeStr callback:(RCTResponseSenderBl
         [self sendResult:RCTMakeError(@"Speech recognition already started!", nil, nil) :nil :nil :nil];
         return;
     }
-
+    
     [SFSpeechRecognizer requestAuthorization:^(SFSpeechRecognizerAuthorizationStatus status) {
         switch (status) {
             case SFSpeechRecognizerAuthorizationStatusNotDetermined:
@@ -235,3 +257,5 @@ RCT_EXPORT_MODULE()
 
 
 @end
+
+
